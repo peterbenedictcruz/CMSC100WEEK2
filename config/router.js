d0dd7ebe851972b1c239c94dbf837f6109931a52
peterbenedictcruz <pscruz4@up.edu.ph>
2015-09-04 12:35:45 +0800

    Edited router.js and student.js

--- config/router.js
+++ config/router.js
@@ -4,6 +4,12 @@ var teacher = require('./../controllers/teacher');
 module.exports = function(router){
 	router.route('/students')
 	.get(student.find)
+	.post(student.insert)
+	
+	router.route('/students/:id')
+	.get(student.findOne)
+	.put(student.update)
+	.delete(student.remove)
 	
 	router.route('/teachers')
 	.get(teacher.find)	
